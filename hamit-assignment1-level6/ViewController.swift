1609df511307c4f8f5743d15f8792bd5e8ab7cdd
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-02-14 15:43:38 -0500

    Initial Commit

---
+++ hamit-assignment1-level6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hamit-assignment1-level6
+//
+//  Created by Hamit Sehjal on 2024-02-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
